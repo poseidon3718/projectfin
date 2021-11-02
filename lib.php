5140e0af0d3cca597b2f72bbc4482af5b51cb10f
poseidon3718 <85330911+poseidon3718@users.noreply.github.com>
2021-11-02 22:22:26 +0900

    Add files via upload

---
+++ lib.php
@@ -0,0 +1,29 @@
+<?php
+  $client_id = "dcmobZVj6clVqXz4iUaa
+";
+  $client_secret = "80g2yM_dgm
+";
+  $encText = urlencode("컴퓨터공학");
+  $url = "https://openapi.naver.com/v1/search/blog.xml?query=".$encText; // json 결과
+//  $url = "https://openapi.naver.com/v1/search/blog.xml?query=".$encText; // xml 결과
+  $is_post = false;
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, $url);
+  curl_setopt($ch, CURLOPT_POST, $is_post);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  $headers = array();
+  $headers[] = "X-Naver-Client-Id: ".$client_id;
+  $headers[] = "X-Naver-Client-Secret: ".$client_secret;
+  curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+  $response = curl_exec ($ch);
+  $status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+  echo "status_code:".$status_code."
+";
+  curl_close ($ch);
+  if($status_code == 200) {
+    echo $response;
+  } else {
+    echo "Error 내용:".$response;
+  }
+?>
